3289f84d92312509102696313b8dc5cb8cd975ad
mustafaikili <mustafaikili@hotmail.com>
2017-09-16 00:17:45 +0300

    Created DBContext

---
+++ HotelReservation.DataAccess/Concreate/EntityFramework/HotelReservationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservation.DataAccess.Concreate.EntityFramework
+{
+    public class HotelReservationDBContext : DbContext
+    {
+        public HotelReservationDBContext():base ("Server=.;Database=MiHotelReservation;Integrated Security=True;")
+        {
+            Database.SetInitializer<HotelReservationDBContext>(new DropCreateDatabaseIfModelChanges<HotelReservationDBContext>());
+        }
+    }
+}
